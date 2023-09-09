34804eb7609dbb1cdfc1bbbab11d3ebd18545925
Simone <simonep97@gmail.com>
2023-09-09 13:31:56 +0200

    first mock version

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,43 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script lang="ts">  
+  let ralValue = 0
+  let result = 0
+
+  const scaglioniIrpef = [
+    {ralFrom: 0, ralTo: 15000, irpef: 23},
+    {ralFrom: 15_001, ralTo: 28_000, irpef: 25},
+    {ralFrom: 28_001, ralTo: 50_000, irpef: 35},
+    {ralFrom: 50_001, ralTo: Infinity, irpef: 43}
+  ]
+
+  function calcolaNetto() {
+    let r = 0
+
+    for (const {irpef, ralFrom, ralTo} of scaglioniIrpef) {            
+      if(ralValue > ralTo){ 
+        r += (ralTo - ralFrom) * (100 - irpef)/100           
+      }else{
+        r += (ralValue - ralFrom) * (100 - irpef)/100
+        break
+      }
+    }
+    
+    result = Math.ceil(r/13)
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Calcolo RAL</h1>
 
   <div class="card">
-    <Counter />
+    <label for="ralInput">Inserisci la RAL:</label>
+    <input type="number" id="ralInput" bind:value={ralValue} />
+
+    <button on:click={calcolaNetto}>CALCOLA RAL</button>
+
+    {#if result > 0}
+      <p>Il netto mensile (13 mensilità) è: {result} €</p>
+    {/if}
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
